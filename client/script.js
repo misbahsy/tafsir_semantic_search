818125077956ac452135e9af98f0e5097f286cc1
Misbah Syed <misbah.sy@gmail.com>
2023-02-03 02:31:50 -0800

    changes

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://127.0.0.1:5000/api/predict', {
+    const response = await fetch('https://quranic-ai.onrender.com/api/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
